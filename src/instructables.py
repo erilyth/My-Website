5f6f5f324918519e115d4a8cc4fa399bc28f9039
erilyth <vishal.batchu@students.iiit.ac.in>
2015-12-03 19:57:06 +0530

    Added python support

---
+++ src/instructables.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2015 vishalapr <vishalapr@vishalapr-Lenovo-G50-70>
+#
+# Distributed under terms of the MIT license.
+
+"""
+
+"""
+import sys
+
+print("HsdasdfI")
+print("YOYO")
+sys.stdout.flush()
--- src/server.js
+++ src/server.js
@@ -10,6 +10,31 @@ var app = express();
 
 app.use(express.static(__dirname+"/public"));
 
+app.get('/instructables', function(reqest, response){
+	var spawn = require('child_process').spawn;
+	var process = spawn('python',["./instructables.py"]);
+	var instructableList = [];
+	process.stdout.on('data',function(data){
+		console.log(data.toString('utf8'));
+		namesList = data.toString('utf8').split(/\r?\n/)
+		console.log(namesList);
+		for (var i=0;i<namesList.length;i++){
+			if (namesList[i]!="")
+			{
+				instructable={
+					name: namesList[i]
+				}
+				instructableList.push(instructable);
+			}
+		}
+	});
+	process.stdout.on('close', function( ){
+		console.log(instructableList);
+		response.json(instructableList);
+	});
+});
+
+
 app.get('/contactlist', function (request,response) { //Creating a new route 'contactlist'
 	console.log("I recieved a get request");
 	person1={
