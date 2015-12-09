7550057b4f8f5f351f3194c16853b779fda7fe19
erilyth <vishal.batchu@students.iiit.ac.in>
2015-12-09 13:28:11 +0530

    Added deploy

---
+++ deploy/src/public/directives/project.js
@@ -0,0 +1,17 @@
+//The <project data=""></project> directive used for displaying projects 
+angular.module('app.directives.project', [])
+	.directive('project', function(){
+		return {
+			restrict: 'E',
+			scope: {
+				projectdata: '='
+			},
+			templateUrl: "templates/directives/project.html",
+			replace: true,
+			controller: function($scope){
+				console.log($scope.projectdata);
+			}
+		};
+	});
+
+//Use link to do DOM related modifications
