7b5d0ee53615e8fcd6d90ab393b45a66a9910375
Erilyth <vishal.batchu@students.iiit.ac.in>
2016-06-08 21:19:38 +0530

    Remove text from the image and add normal text on homepage

--- src/public/scroll.js
+++ src/public/scroll.js
@@ -23,25 +23,25 @@ $(function () {
   		start: 'Images/development.png',
   		end: 'Images/developmentblur.png',
   		backgroundPosition: 'center center',
-  		threshold: 0.2
+  		threshold: 0.7
   });
   $('.contact').crossfade({
       start: 'Images/background.png',
       end: 'Images/background.png',
       backgroundPosition: 'center center',
-      threshold: 0.2
+      threshold: 0.7
   });
   $('.about').crossfade({
       start: 'Images/background.png',
       end: 'Images/background.png',
       backgroundPosition: 'center center',
-      threshold: 0.2
+      threshold: 0.7
   });
   $('.boundaryDivider').crossfade({
       start: 'Images/boundary.jpg',
       end: 'Images/boundary.jpg',
       backgroundPosition: 'center center',
-      threshold: 0.2
+      threshold: 0.7
   })
 });
 
